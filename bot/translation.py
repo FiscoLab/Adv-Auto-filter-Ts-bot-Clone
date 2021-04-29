a5a4be3f0f42fc37e4c3bcdc0994c32bf5e46115
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-04-29 14:57:30 +0530

    translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @v_spot
+# (c) @Diago_x
 
 class Translation(object):
     
@@ -10,45 +10,51 @@ class Translation(object):
  ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <b><i><a href="https://t.me/Film_zone_group">Film_Zone™</a></i></b>
 ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
 
-🔱 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 @V_spot
+🔱 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 @DIAGO_X
 </i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 <i>
-⭕ <b>Add Me To Any Group And Make Me Admin</b>
-⭕ <b>Add Me To Your Desired Channel</b>
+◾ <b>Add Me To Any Group And Make Me Admin</b>
+◾<b>Add Me To Your Desired Channel</b>
 </i>
 <b>Bot Commands (Works Only In Groups) :</b>
-    🔷 <code>/add chat_id</code>
+   
+ 🍁 <code>/add chat_id</code>
                 OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
+ 🍁 <code>/add @Username</code>
      
-    🔷 <code>/del chat_id</code>
+ 🍁 <code>/del chat_id</code>
                 OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
+ 🍁 <code>/del @Username</code>
      
-    🔷 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+ 🍁 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    🔷 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-            🔷 <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+ 🍁 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+    
+ 🍁<code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
-            🔷 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+ 🍁 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
             
-🔷 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+ 🍁 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
-            🔷 <code>Status</code> - Button Will Shows The Stats Of Your Channel
+ 🍁 <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@v_spot
+*[©FISCO LAB](https://t.me/fisco_lab)*
 """
     
-    ABOUT_TEXT = """<b>💠 My Name</b> : <code> Fz Links </code>
+ • My Name     : OMEGA
     
-<b>💠 Creator</b> : <b><i><a href="https://t.me/v_spot">v_spot</a></i></b>
+ • Creator     : [DIAGO](https://t.me/DIAGO_X)&[FICO LAB](https://t.me:/FISCO_LAB)
 
-<b>💠 Language</b> : <code>Python3</code>
+ • Language    : Python3
 
-<b>💠 Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram</a></i>
+ • Server      : Heroku
+ 
+ • Library     : pyrogram
 
-<b>💠 Source Code</b> : <i>🔐</i>
+ • Source Code : 🔐
 """
+            
+[©FISCO LAB](https://t.me/fisco_lab)
