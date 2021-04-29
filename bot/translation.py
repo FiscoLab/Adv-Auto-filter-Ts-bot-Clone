adadb9439e14dcb2b0c11a9d1dd4446ef46a3d64
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-04-29 15:17:42 +0530

    translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,58 +1,70 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @Diago_x
+# (c) @AlbertEinsteinTG | @Hillard_Har 
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i> How are you 🤷‍♂
+    START_TEXT = """
+😅 Hai {}, 
 
- ɪ ᴀᴍ ᴀ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ sɪᴍᴘʟʏ sᴇᴀʀᴄʜ ғᴏʀ ᴛʜᴇ ғɪʟᴇs ᴍᴀᴅᴇ ғᴏʀ <b><i><a href="https://t.me/Film_zone_group">Film_Zone™</a></i></b>
-ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ɢɪᴠᴇɴ ᴏ̨ᴜᴇʀʏ ᴀɴᴅ ɢɪᴠᴇs ʟɪɴᴋ ᴛᴏ ᴛʜᴏsᴇ ғɪʟᴇs ᴀs ʙᴜᴛᴛᴏɴs
+I am a filter bot with advanced features 
+currently working for **[Film_Zone](https://t.me/film_zone_group)**
 
-🔱 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ 👉 @DIAGO_X
-</i>"""    
+⚜️ 𝐌𝐚𝐢𝐧𝐭𝐚𝐢𝐧𝐞𝐝 𝐁𝐲 👉 **[DIAGO](https://t.me/diago_x)**
+"""   
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<u>💡 𝐇𝐞𝐥𝐩</u>
+
 <i>
-◾ <b>Add Me To Any Group And Make Me Admin</b>
-◾<b>Add Me To Your Desired Channel</b>
+📌 Add Me To Any Group And Make Me Admin
+📌 Add Me To Your Desired Channel
 </i>
-<b>Bot Commands (Works Only In Groups) :</b>
-   
- 🍁 <code>/add chat_id</code>
+
+<b>🔰 𝐌𝐲 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 (Works Only In Groups) :</b>
+
+    👉 <code>/add chat_id</code>
                 OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
- 🍁 <code>/add @Username</code>
+     <code>/add @Username</code>
      
- 🍁 <code>/del chat_id</code>
+    👉 <code>/del chat_id</code>
                 OR                  - To disconnect A Group With A Channel
- 🍁 <code>/del @Username</code>
+     <code>/del @Username</code>
      
- 🍁 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    👉 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
- 🍁 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-    
- 🍁<code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+    👉 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+🔰 𝐒𝐄𝐓𝐓𝐈𝐍𝐆 𝐏𝐀𝐍𝐄𝐋
+
+            👉 <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
- 🍁 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+            👉 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            👉 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
- 🍁 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            👉 <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
- 🍁 <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-*[©FISCO LAB](https://t.me/fisco_lab)*
+<b><a href="https://t.me/fisco_lab">©️ Fisco Lab</a></b>
 """
     
- • Name     : Omega-Filter-Bot
-    
- • Creator     : [DIAGO](https://t.me/DIAGO_X)&[FICO LAB](https://t.me:/FISCO_LAB)
+    ABOUT_TEXT = """
+📕 𝐀𝐛𝐨𝐮𝐭 𝐌𝐞 ,
+\n○ ᴍʏ ɴᴀᴍᴇ : omega
 
- • Language    : Python3
- 
- • Library     : pyrogram
+○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
 
- • Source Code : 🔐
+○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
+
+○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
+
+○ ᴠᴇʀsɪᴏɴ : 1.0.0
+
+○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : 🔐
+
+○ ᴄʀᴇᴀᴛᴏʀ : [DIAGO](https://t.me/diago_x)
+
+**ᴜᴘᴅᴀᴛᴇᴅ ᴏɴ 26-4-21 ɪɴᴅɪᴀɴ ᴛɪᴍᴇ 4:50 ᴘᴍ**
+
+**[©FISCO_LAB](https://t.me/Fisco_lab)**
 """
-            
-[©FISCO LAB](https://t.me/fisco_lab)
