754bfb52a890abb02df3e0af0a2c137989ffe196
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-05-01 23:13:35 +0530

    autofilter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -184,8 +184,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"**😃 Got it \n\nYour Query 👉 {query}**
-                ©Film zone",
+                text=f"**😃 Got it \n\nYour Query 👉 {query}** \n©Film zone",
                 reply_markup=reply_markup,
                 parse_mode="markdown",
                 reply_to_message_id=update.message_id
