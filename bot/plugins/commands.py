6725ef54c071b0bfdc7c92f7b057dd3d639ae245
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-05-01 15:24:50 +0530

    commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -24,7 +24,15 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + """</code>\n<b>━━━━━━━━━━━━━━━━━━━━
+🌐 ɢʀᴏᴜᴘ
+@Groupname
+  
+💠ᴄʜᴀɴɴᴇʟ
+@channelname
+
+💠 ᴄʜᴀɴɴᴇʟs
+@channelname</b>""")
         
         if file_type == "document":
         
