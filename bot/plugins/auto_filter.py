381edabcbea2e4aba0fc3dad30138d98af3b6cc9
FiscoLab <83391319+FiscoLab@users.noreply.github.com>
2021-05-03 17:28:20 +0530

    auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -106,7 +106,7 @@ async def auto_filter(bot, update):
                 ]
             )
         
-    else:
+    else:await bot.send_message(update.chat.id, "check spelling")
         return # return if no files found for that query
     
 
